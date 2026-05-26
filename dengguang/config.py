5e0e03296b1a88534e3923f404053467f6fc5826
User <user@example.com>
2026-05-26 15:26:42 +0800

    feat(config): add chinese font support for cross-platform
    add os import, scan common chinese font paths, fall back to default font if no chinese font found

--- dengguang/config.py
+++ dengguang/config.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -29,9 +30,42 @@ FONT_SIZE_LARGE = 36
 FONT_SIZE_XLARGE = 48
 
 pygame.font.init()
-FONT_SMALL = pygame.font.Font(None, FONT_SIZE_SMALL)
-FONT_MEDIUM = pygame.font.Font(None, FONT_SIZE_MEDIUM)
-FONT_LARGE = pygame.font.Font(None, FONT_SIZE_LARGE)
-FONT_XLARGE = pygame.font.Font(None, FONT_SIZE_XLARGE)
+
+def get_chinese_font():
+    font_paths = [
+        "C:/Windows/Fonts/simhei.ttf",
+        "C:/Windows/Fonts/simsun.ttc",
+        "C:/Windows/Fonts/msyh.ttc",
+        "C:/Windows/Fonts/kaiu.ttf",
+        "C:/Windows/Fonts/fangsong.ttf",
+        "/Library/Fonts/SimHei.ttf",
+        "/Library/Fonts/Songti.ttc",
+        "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc",
+        "/usr/share/fonts/truetype/arphic/ukai.ttc",
+    ]
+    
+    for font_path in font_paths:
+        if os.path.exists(font_path):
+            return font_path
+    return None
+
+CHINESE_FONT_PATH = get_chinese_font()
+
+if CHINESE_FONT_PATH:
+    try:
+        FONT_SMALL = pygame.font.Font(CHINESE_FONT_PATH, FONT_SIZE_SMALL)
+        FONT_MEDIUM = pygame.font.Font(CHINESE_FONT_PATH, FONT_SIZE_MEDIUM)
+        FONT_LARGE = pygame.font.Font(CHINESE_FONT_PATH, FONT_SIZE_LARGE)
+        FONT_XLARGE = pygame.font.Font(CHINESE_FONT_PATH, FONT_SIZE_XLARGE)
+    except:
+        FONT_SMALL = pygame.font.Font(None, FONT_SIZE_SMALL)
+        FONT_MEDIUM = pygame.font.Font(None, FONT_SIZE_MEDIUM)
+        FONT_LARGE = pygame.font.Font(None, FONT_SIZE_LARGE)
+        FONT_XLARGE = pygame.font.Font(None, FONT_SIZE_XLARGE)
+else:
+    FONT_SMALL = pygame.font.Font(None, FONT_SIZE_SMALL)
+    FONT_MEDIUM = pygame.font.Font(None, FONT_SIZE_MEDIUM)
+    FONT_LARGE = pygame.font.Font(None, FONT_SIZE_LARGE)
+    FONT_XLARGE = pygame.font.Font(None, FONT_SIZE_XLARGE)
 
 MAX_HINTS = 3
